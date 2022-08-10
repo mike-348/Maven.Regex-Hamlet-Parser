179ffd0ccc495ea88005b9874df508d0690ad006
Mike <mike@Zipcoders-MacBook-Pro-41.local>
2022-08-10 16:47:08 -0400

    lab complete

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -34,6 +36,60 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String replaceHamletWithLeon(String text) {
+
+//        Pattern pattern = Pattern.compile("hamlet", Pattern.CASE_INSENSITIVE);
+        Pattern pattern1 = Pattern.compile("Hamlet");
+        Matcher matcher1 = pattern1.matcher(text);
+        String result = matcher1.replaceAll("Leon");
+
+        Pattern pattern2 = Pattern.compile("HAMLET");
+        Matcher matcher2 = pattern2.matcher(result);
+        result = matcher2.replaceAll("LEON");
+        return result;
+    }
+
+    public String replaceHoratioWithTariq(String text) {
+
+//        Pattern pattern = Pattern.compile("horatio", Pattern.CASE_INSENSITIVE);
+        Pattern pattern1 = Pattern.compile("Horatio");
+        Matcher matcher1 = pattern1.matcher(text);
+        String result = matcher1.replaceAll("Tariq");
+
+        Pattern pattern2 = Pattern.compile("HORATIO");
+        Matcher matcher2 = pattern2.matcher(result);
+        result = matcher2.replaceAll("TARIQ");
+        return result;
+    }
+
+    public int findHamlet(String text) {
+        Pattern pattern1 = Pattern.compile("Hamlet");
+        Pattern pattern2 = Pattern.compile("HAMLET");
+
+        Matcher matcher1 = pattern1.matcher(text);
+        Matcher matcher2 = pattern2.matcher(text);
+
+        if (matcher1.find()) {
+            return 1;
+        } else if (matcher2.find()) {
+            return 2;
+        } else return -1;
+    }
+
+    public int findHoratio(String text) {
+        Pattern pattern1 = Pattern.compile("Horatio");
+        Pattern pattern2 = Pattern.compile("HORATIO");
+
+        Matcher matcher1 = pattern1.matcher(text);
+        Matcher matcher2 = pattern2.matcher(text);
+
+        if (matcher1.find()) {
+            return 1;
+        } else if (matcher2.find()) {
+            return 2;
+        } else return -1;
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,107 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        //given
+        String input = "The Tragedy of Hamlet, Prince of Denmark";
+        String expected = "The Tragedy of Leon, Prince of Denmark";
+
+        //when
+        String actual = hamletParser.replaceHamletWithLeon(input);
+
+        //then
+        Assert.assertEquals(expected,actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        //given
+        String input = "Thou art a scholar; speak to it, Horatio.";
+        String expected = "Thou art a scholar; speak to it, Tariq.";
+
+        //when
+        String actual = hamletParser.replaceHoratioWithTariq(input);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHamletToLeonUppercase() {
+        //given
+        String input = "Enter HAMLET";
+        String expected = "Enter LEON";
+
+        //when
+        String actual = hamletParser.replaceHamletWithLeon(input);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHoratioToLeonUppercase() {
+        //given
+        String input = "Enter HORATIO, MARCELLUS, and BERNARDO";
+        String expected = "Enter TARIQ, MARCELLUS, and BERNARDO";
+
+        //when
+        String actual = hamletParser.replaceHoratioWithTariq(input);
+
+        //then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHoratio() {
+        //given
+        String input = "Enter HORATIO, MARCELLUS, and BERNARDO";
+        int expected = 2;
+
+        //when
+        int actual = hamletParser.findHoratio(input);
+
+        //then
+        Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testFindHamlet() {
+        //given
+        String input = "The Tragedy of Hamlet, Prince of Denmark";
+        int expected = 1;
+
+        //when
+        int actual = hamletParser.findHamlet(input);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReplaceAllHamlet() {
+        //given
+        String input = hamletText;
+        int expected = -1;
+
+        //when
+        String alteredText = hamletParser.replaceHamletWithLeon(input);
+        int actual = hamletParser.findHamlet(alteredText);
+
+        //then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testReplaceAllHoratio() {
+        //given
+        String input = hamletText;
+        int expected = -1;
+
+        //when
+        String alteredText = hamletParser.replaceHoratioWithTariq(input);
+        int actual = hamletParser.findHoratio(alteredText);
+
+        //then
+        Assert.assertEquals(expected, actual);
     }
 }
